b0535e59da7912df9c941c5c4344902042a76314
Octavian Regatun <octavian.regatun@outlook.com>
2020-06-21 15:39:00 +0300

    travis setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { DatePicker } from 'antd';
+
 import 'antd/dist/antd.css';
 
 class App extends React.PureComponent {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <>
+        <h1>Hello World</h1>
+      </>
+    );
   }
 }
 
