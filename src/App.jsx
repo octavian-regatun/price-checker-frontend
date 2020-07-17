271c10b4c5163ae9b4d9edf5d897ee269835d917
Octavian Regatun <octavian.regatun@outlook.com>
2020-07-17 14:17:46 +0300

    made landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 
-import 'antd/dist/antd.css';
+import { withStyles, createMuiTheme } from '@material-ui/core/styles';
+
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+import Navbar from './components/Navbar';
+import { Search } from './components/Search';
+import { Header } from './components/Header';
+
+const theme = createMuiTheme();
+
+const styles = (theme) => ({
+  toolbar: theme.mixins.toolbar
+});
 
 class App extends React.PureComponent {
   render() {
+    const { classes } = this.props;
     return (
-      <>
-        <h1>Hello Worldd</h1>
-      </>
+      <div style={{ height: '100vh' }}>
+        <Navbar />
+        <Grid container item xs={12}>
+          <Header />
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          justify='center'
+          style={{ marginTop: '-32px' }}
+        >
+          <Search />
+        </Grid>
+      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
