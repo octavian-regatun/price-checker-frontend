3c8fe3ab7fc25a934edcb25d67a4fd79d64f8989
Octavian Regatun <octavian.regatun@outlook.com>
2020-07-22 14:23:14 +0300

    add search button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,30 @@
 import React, { PureComponent } from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
+import Axios from 'axios';
 
 const styles = () => ({});
 
 export class Search extends PureComponent {
+  state = {
+    search: ''
+  };
+
+  onSearchButtonClick = () => {
+    Axios.get(
+      `https://or-price-checker-backend.herokuapp.com/products/name/${this.state.search}`
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   render() {
-    const { classes } = this.props;
+    // const { classes } = this.props;
     return (
       <>
         <TextField
@@ -15,7 +32,17 @@ export class Search extends PureComponent {
           label='Caută produsul'
           variant='filled'
           style={{ backgroundColor: 'white', borderRadius: '5px' }}
+          onChange={(e) => {
+            this.setState({ search: e.target.value });
+          }}
         />
+        <Button
+          variant='contained'
+          style={{ marginLeft: '16px' }}
+          onClick={this.onSearchButtonClick}
+        >
+          Caută
+        </Button>
       </>
     );
   }
